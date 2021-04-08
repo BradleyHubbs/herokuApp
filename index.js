95eff38810f21bd886980ca0ad021415376463bf
BradleyHubbs <75264076+BradleyHubbs@users.noreply.github.com>
2021-04-08 13:52:45 -0400

    react app

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const dotenv = require('dotenv').config()
 const express = require('express');
+const cors = require('cors') 
 const app = express()
 const port = process.env.PORT || 9000 // allows the use of heroku or deployment services env.port
+const path = require('path');
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use("/api/", (_,res)=>{
     res.json({data:"API is accounted for"})
